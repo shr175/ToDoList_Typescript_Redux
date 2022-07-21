226f16ce75968a3c6fe7b0d05459cc0f26d01259
Shreya Nayak <shreyanayak@Shreyas-MacBook-Air.local>
2022-07-21 11:30:47 +0530

    some chnages

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+import Create from './components/ToDoList';
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+return (
+
+  <>
+  
+  <Navbar />
+  <Create />
+
+ 
+  </>
+
+  );
+
+
+}
+
+export default App;
+
